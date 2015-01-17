12fa4f84cad050d3429ef0c5052563ec39ddd550
Dima <ImphitaminXenon@gmail.com>
2015-01-17 21:27:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dmitry/sherlock/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.dmitry.sherlock;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    static final private int WHO_THIEF = 0;
+
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        startActivityForResult(intent, WHO_THIEF);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        TextView InfoAboutThief = (TextView)findViewById(R.id.answer_info);
+
+        if (requestCode == WHO_THIEF) {
+            if (resultCode == RESULT_OK) {
+                String thiefname = data.getStringExtra(SecondActivity.THIEF);
+                InfoAboutThief.setText(thiefname);
+            } else {
+                InfoAboutThief.setText("");
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/dmitry/sherlock/SecondActivity.java
@@ -0,0 +1,43 @@
+package com.example.dmitry.sherlock;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by Dmitry on 16.01.2015.
+ */
+public class SecondActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    public final static String THIEF = "com.example.dmitry.sherlock.THIEF";
+
+    public void onRadioClick(View v) {
+        Intent answer_intent = new Intent();
+
+        switch (v.getId()) {
+            case R.id.radioPapa:
+                answer_intent.putExtra(THIEF, "Папа");
+                break;
+            case R.id.radioMama:
+                answer_intent.putExtra(THIEF, "Мама");
+                break;
+            case R.id.radioDanya:
+                answer_intent.putExtra(THIEF, "Сам всё съел");
+                break;
+
+            default:
+                break;
+
+        }
+
+        setResult(RESULT_OK, answer_intent);
+        finish();
+    }
+}
